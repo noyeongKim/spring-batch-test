ca1adbe9c09d177e65103f2786bac02479546b55
noyeongKim <househusband813@gmail.com>
2022-12-20 18:27:48 +0900

    h2 database 적용

--- src/main/java/com/kino/springbatchtest/SpringBatchTestApplication.java
+++ src/main/java/com/kino/springbatchtest/SpringBatchTestApplication.java
@@ -1,9 +1,11 @@
 package com.kino.springbatchtest;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing  // spring batch 사용을 위한 어노테이션 추가
 public class SpringBatchTestApplication {
 
     public static void main(String[] args) {
