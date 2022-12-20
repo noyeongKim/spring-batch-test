987fb25d12b2987fcef31bc734725633c9df6b21
noyeongKim <househusband813@gmail.com>
2022-12-20 17:11:58 +0900

    init

---
+++ src/test/java/com/kino/springbatchtest/SpringBatchTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kino.springbatchtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
